54a33aa0e58816f274a7f17cb772c7ddf9fb09ff
MehZehLee <kjmazely@gmail.com>
2025-10-20 13:21:45 -0400

    test

--- StudentTrackerApp/obj/Debug/net8.0/StudentTrackerApp.AssemblyInfo.cs
+++ StudentTrackerApp/obj/Debug/net8.0/StudentTrackerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentTrackerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21d23b7b031daab1327c330d570ae06f74327022")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f29b78c89d4f1b3aa1a50577ce2db0407a0ec990")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentTrackerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentTrackerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
