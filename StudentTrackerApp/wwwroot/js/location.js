190da45d7a3aecf3e892bb88e8f9b7ca06c91b5e
KJMazely <kjmazely@gmail.com>
2025-10-27 14:57:44 -0400

    Added location JS file

---
+++ StudentTrackerApp/wwwroot/js/location.js
@@ -0,0 +1,19 @@
+﻿window.getUserLocation = async function () {
+    return new Promise((resolve, reject) => {
+        if (!navigator.geolocation) {
+            reject('Geolocation not supported by this browser.');
+        } else {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    resolve({
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    });
+                },
+                error => {
+                    reject(error.message);
+                }
+            );
+        }
+    });
+};
