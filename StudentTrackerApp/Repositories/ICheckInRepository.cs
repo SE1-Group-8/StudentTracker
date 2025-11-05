73d736875b47cc0ca994c63597d2526f406c54b8
KJMazely <kjmazely@gmail.com>
2025-11-05 14:25:35 -0500

    Created student check in log

---
+++ StudentTrackerApp/Repositories/ICheckInRepository.cs
@@ -0,0 +1,12 @@
+﻿using StudentTrackerApp.Models;
+
+namespace StudentTrackerApp.Repositories
+{
+    public interface ICheckInRepository
+    {
+        Task<CheckInLog> CreateCheckInAsync(CheckInLog log);
+        Task<CheckInLog?> GetActiveCheckInAsync(int userId);
+        Task UpdateCheckOutAsync(CheckInLog log);
+        Task<List<CheckInLog>> GetLogsByUserIdAsync(int userId);
+    }
+}
